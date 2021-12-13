a5d2e1c6dc017c19e5e1fd4b51418bb2d24aea80
Anju <88181896+ansanigom@users.noreply.github.com>
2021-12-13 12:19:00 +0900

    form js and styles

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,11 @@
 import 'bootstrap';
+
+const form = document.querySelector('.form');
+
+const formSubmit = (event) => {
+  event.preventDefault();
+  form.submit();
+  form.reset();
+};
+
+form.addEventListener('submit', formSubmit);
