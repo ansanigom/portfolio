0dc67d2930115c3d97056b52c0249a1e0e00cab9
Anju <88181896+ansanigom@users.noreply.github.com>
2021-12-15 03:04:09 +0900

    sticky js added

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -9,3 +9,23 @@ const formSubmit = (event) => {
 };
 
 form.addEventListener('submit', formSubmit);
+
+// position: sticky
+const stickyElem = document.querySelector('.hider');
+
+/* Gets the amount of height
+of the element from the
+viewport and adds the
+pageYOffset to get the height
+relative to the page */
+const currStickyPos = stickyElem.getBoundingClientRect().top + window.pageYOffset;
+
+window.onscroll = function () {
+  if (window.pageYOffset > currStickyPos) {
+    stickyElem.style.position = 'fixed';
+    stickyElem.style.top = '0px';
+  } else {
+    stickyElem.style.position = 'relative';
+    stickyElem.style.top = 'initial';
+  }
+};
