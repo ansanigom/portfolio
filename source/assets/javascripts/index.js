acefbb578e1d0bb2d166329edb01284499345be7
Anju <88181896+ansanigom@users.noreply.github.com>
2021-12-16 15:11:16 +0900

    styles and js

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -12,39 +12,22 @@ form.addEventListener('submit', formSubmit);
 
 
 // view more
+function myFunction() {
+  const moreTextOne = document.getElementById("one");
+  const moreTextTwo = document.getElementById("two");
+  const btnText = document.getElementById("myBtn");
 
+  if (moreTextTwo.style.display === "none") {
+    moreTextTwo.style.display = "inline";
+    btnText.innerHTML = "Read more";
+  }
 
-const numberListLen = (data.products).length;
-const defaultNum = 3;
-const addNum = 3;
-let currentNum = 0;
-
-(data.products).each(function () {
-  (this).find('#more_btn').show();
-  (this).find('#close_btn').hide();
-
-  (this).find('li:not(:lt(" + defaultNum + "))').hide();
-
-  currentNum = defaultNum;
-
-  ('#more_btn').click(function () {
-    currentNum += addNum;
-
-    (this).parent().find(data.products(' + currentNum + ')).slideDown();
-
-    if (numberListLen <= currentNum) {
-      currentNum = defaultNum;
-      const indexNum = currentNum - 1;
-
-      ('#more_btn').hide();
-      ('#close_btn').show();
-
-      ('#close_btn').click(function () {
-        (this).parent().find(data.products(' + indexNum + ')).slideUp();
-
-        (this).hide();
-        ('#more-btn').show();
-      });
-    }
-  });
-});
+  if (moreTextOne.style.display === "none") {
+    moreTextOne.style.display = "inline";
+    btnText.innerHTML = "Read more";
+  } else {
+    moreTextTwo.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreTextTwo.style.display = "inline";
+  }
+}
